54e6985d5ae10b38a0f4cdbd3b99ea078a4970fa
king jhonny <kingjhonny@gmail.com>
2024-10-07 17:41:08 +0530

    Update Main.java and add new JAR file

--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,48 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Prompt for database connection details
+        System.out.print("Enter DB URL (e.g., jdbc:mysql://mysql-container:3306/mydb): ");
+        String url = scanner.nextLine();
 
+        System.out.print("Enter DB Username: ");
+        String username = scanner.nextLine();
 
-            private static final String url ="jdbc:mysql://localhost:3306/mydb";
-            private static final String username="root";
-            private static final String password="";
+        System.out.print("Enter DB Password: ");
+        String password = scanner.nextLine();
 
-            public static void main(String[] args) {
-                try {
-                    String className = "com.mysql.cj.jdbc.Driver";
-                    Class.forName(className);
+        try {
+            String className = "com.mysql.cj.jdbc.Driver";
+            Class.forName(className);
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM students";
+            ResultSet resultSet = statement.executeQuery(query);
 
-
-                } catch (Exception e){
-                    System.out.println(e.getMessage());
-                }
-                try {
-                    Connection connection = DriverManager.getConnection(url, username, password);
-                    Statement statement = connection.createStatement();
-                    String query = "select * from students";
-                    ResultSet resultSet=statement.executeQuery(query);
-                    while(resultSet.next()){
-                        int id = resultSet.getInt("id");
-                        String name = resultSet.getString("name");
-                        int roll = resultSet.getInt("roll");
-                        double marks = resultSet.getDouble("marks");
-                        System.out.println("Id:"+id);
-                        System.out.println("Name:"+name);
-                        System.out.println("Roll:"+roll);
-                        System.out.println("Marks:"+marks);
-                    }
-                }catch (Exception e){
-                    System.out.println(e.getMessage());
-                }
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int roll = resultSet.getInt("roll");
+                double marks = resultSet.getDouble("marks");
+                System.out.println("Id: " + id);
+                System.out.println("Name: " + name);
+                System.out.println("Roll: " + roll);
+                System.out.println("Marks: " + marks);
             }
-        }
 
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
